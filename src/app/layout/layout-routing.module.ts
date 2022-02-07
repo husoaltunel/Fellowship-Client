0dedc651e779e5b234eaf942f2a947be2779ecf1
Hüseyin ALTUNEL <65026269+husoaltunel@users.noreply.github.com>
2022-02-07 16:40:44 +0300

    home page edited

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,8 +8,7 @@ import { AuthGuard } from '../guards/auth.guard';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
-  { path: "", component: LayoutComponent },
-  { path: "home", component: LayoutComponent, loadChildren: () => import("./components/home/home.module").then(m => m.HomeModule) },
+  { path: "", component: LayoutComponent, loadChildren: () => import("./components/home/home.module").then(m => m.HomeModule) },
   { path: "register", component: LayoutComponent, loadChildren: () => import("./components/register/register.module").then(m => m.RegisterModule) },
   { path: "lists", component: LayoutComponent, loadChildren: () => import("./components/lists/lists.module").then(m => m.ListsModule), canActivate: [AuthGuard]  },
   { path: "user-list", component: LayoutComponent, loadChildren: () => import("./components/users/user-list/user-list.module").then(m => m.UserListModule)  ,canActivate:[AuthGuard]},
